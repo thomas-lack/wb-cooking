5da130a41f8ee04a63881bddb003775fca97584f
Thomas Lack <post.thomas.lack@gmail.com>
2022-10-25 00:12:58 +0200

    Add list of available recipes

--- src/WbCooking.svelte
+++ src/WbCooking.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import RecipesList from './pages/recipes-list/RecipesList.svelte';
 </script>
 
 <main>
   <h1>wb cooking</h1>
+
+  <RecipesList />
 </main>
 
 <style lang="scss">
---
+++ src/pages/recipes-list/RecipesList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Recipe } from '../../shared/store/recipes/recipe'
+  import { recipes } from '../../shared/store/recipes/recipes.store'
+
+  let recipesList: Recipe[] = []
+
+  const getAll = recipes.getAll
+
+  $: recipesList = $getAll
+</script>
+
+<h2>Rezepte:</h2>
+
+<ul>
+  {#each recipesList as recipe}
+    <li>{recipe.name}</li>
+  {/each}
+</ul>
+
+<style lang="scss">
+</style>
