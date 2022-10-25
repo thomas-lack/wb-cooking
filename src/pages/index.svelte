0daace4f990ca99aa15987529102943d28e41baa
Thomas Lack <post.thomas.lack@gmail.com>
2022-10-26 01:01:18 +0200

    add default page

---
+++ src/pages/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<h1><img class="logo" src="/assets/cooking_icon.svg" alt="logo">wb cooking</h1>
+
+<style lang="scss">
+  .logo {
+    width: 50px;
+    margin-right: 10px;
+  }
+</style>
