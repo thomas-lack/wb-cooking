0e5dd6d41b227d77492b402aa804e7fe44de3f8f
Thomas Lack <post.thomas.lack@gmail.com>
2022-10-26 01:00:39 +0200

    Add router

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import sveltePreprocess from 'svelte-preprocess'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import pages from 'vite-plugin-pages-svelte';
 
 // https://vitejs.dev/config/
-const production = process.env.NODE_ENV === 'production'
-const sourceMapsInProduction = false
+const production = process.env.NODE_ENV === 'production';
+const sourceMapsInProduction = false;
 export default defineConfig({
   plugins: [
     svelte({
       emitCss: production,
       preprocess: sveltePreprocess(),
       compilerOptions: {
-        dev: !production
-      }
-    })
+        dev: !production,
+      },
+    }),
+    pages(),
   ],
   server: {
     host: 'localhost',
-    port: 4200
+    port: 4200,
   },
   build: {
-    sourcemap: sourceMapsInProduction
+    sourcemap: sourceMapsInProduction,
   },
 });
