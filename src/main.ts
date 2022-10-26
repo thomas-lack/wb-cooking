5de61ccc6c4c4a3de784ca903379172d105119ed
Thomas Lack <post.thomas.lack@gmail.com>
2022-10-27 00:41:46 +0200

    Add chrome redux plugin support

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
-import { get } from 'svelte/store';
-import { recipes } from './shared/store/recipes/recipes.store';
+import { derived, get } from 'svelte/store';
+import { appConfig } from './shared/stores/app-config/app-config.store';
+import { recipes } from './shared/stores/recipes/recipes.store';
 import './wb-cooking.css';
 import App from './WbCooking.svelte';
+import { reduxify } from 'svelte-reduxify';
+import { weeklyPlan } from './shared/stores/weekly-plan/weekly-plan.store';
 
 const app = new App({
   target: document.getElementById('wb-cooking'),
 });
+
+// show selected stores in redux devtools browser plugin
+if (get(appConfig.devMode)) {
+  reduxify(
+    derived(
+      [appConfig, weeklyPlan, recipes],
+      ([$appConfig, $weeklyPlan, $recipes]) => ({
+        appConfig: $appConfig,
+        weeklyPlan: $weeklyPlan,
+        recipes: $recipes,
+      })
+    )
+  );
+}
 
 recipes
   .readFromJson()
