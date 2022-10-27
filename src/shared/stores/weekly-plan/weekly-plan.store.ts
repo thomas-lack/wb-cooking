f0b7367d49e52d5d6b46a95959edaa00e6557461
Thomas Lack <post.thomas.lack@gmail.com>
2022-10-28 00:59:03 +0200

    Add total ingredients stub, add various calculations

--- src/shared/stores/weekly-plan/weekly-plan.store.ts
+++ src/shared/stores/weekly-plan/weekly-plan.store.ts
@@ -1,4 +1,5 @@
 import { derived, get, writable } from 'svelte/store';
+import type { Ingredient } from '../recipes/ingredient';
 import type { Recipe } from '../recipes/recipe';
 import { recipes } from '../recipes/recipes.store';
 
@@ -22,8 +23,23 @@ export const weeklyPlan = {
   totalRecipes,
   portions,
   recipesList: derived(recipesList, ($recipesList) => $recipesList),
+  modifyRecipePortions,
   generate,
+  getTotalIngredients: derived(recipesList, ($recipesList) =>
+    getTotalIngredients($recipesList)
+  ),
 };
+
+function modifyRecipePortions(recipe: Recipe, value: number) {
+  recipesList.set(
+    get(recipesList).map((r) => {
+      if (r.id === recipe.id) {
+        r.calculatedPortions = value;
+      }
+      return r;
+    })
+  );
+}
 
 function generate() {
   recipesList.set(recipes.getDistinctSet(get(totalRecipes)));
@@ -31,5 +47,37 @@ function generate() {
 }
 
 function distributePortionAmounts() {
-  // TODO
+  const portionDistribution = getPortionDistribution();
+  recipesList.set(
+    get(recipesList).map((recipe, i) => {
+      recipe.calculatedPortions = portionDistribution[i];
+      return recipe;
+    })
+  );
+}
+
+function getPortionDistribution(): number[] {
+  let restPortions = get(portions);
+  let i = 0;
+  const portionDistribution: number[] = Array.from(
+    { length: get(totalRecipes) },
+    () => 0
+  );
+  while (restPortions > 0) {
+    portionDistribution[i % get(totalRecipes)] =
+      portionDistribution[i % get(totalRecipes)] + 1;
+    restPortions--;
+    i++;
+  }
+  return portionDistribution;
+}
+
+function getTotalIngredients(recipes: Recipe[]): Ingredient[] {
+  let ingredients: Ingredient[] = [];
+  recipes.map((recipe) => {
+    ingredients = ingredients.concat(recipe.ingredients);
+    //TODO sum up same ingredients
+    //TODO calculate relative ingredients from portions
+  });
+  return ingredients;
 }
